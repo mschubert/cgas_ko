5b5ee646c7197e064979730edb210f6cef1489ab
mschubertv <mschu.dev@gmail.com>
2020-11-06 20:07:30 +0000

    better gene plots

--- rev_effect/plot.r
+++ rev_effect/plot.r
@@ -6,23 +6,57 @@ sys = import('sys')
 plt = import('plot')
 util = import('./util')
 
-volcano_or_compare = function(ref, cmp) {
+volcano_or_compare = function(ref, cmp, col="genes", thresh=2.5, label_base=150, hl=c(), hollow=c()) {
     if (ref == cmp)
-        return(volcano(ref))
+        return(volcano(ref, col, hl))
 
-    sr = rlang::sym(ref)
-    sc = rlang::sym(cmp)
-    rdf = inner_join(res[[ref]] %>% transmute(label, !! sr := stat),
-                     res[[cmp]] %>% transmute(label, !! sc := stat)) %>%
-        filter((!! sr)^2 + (!! sc)^2 > 2.5^2)
+    sym = list(x=rlang::sym(cmp), y=rlang::sym(ref), col=rlang::sym(col))
+    refdf = res %>% filter(cond == ref) %>% pull(!! sym$col) %>% `[[`(1)
+    cmpdf = res %>% filter(cond == cmp) %>% pull(!! sym$col) %>% `[[`(1)
+
+    avg_chr_label = mean(nchar(refdf$label), na.rm=TRUE)
+    topf = function(dist, frac)
+        rank(-dist, ties.method="random") < frac*label_base/sqrt(avg_chr_label)
+    colors = setNames(c("#a50f1567", "#006d2c67", "#045a8d67", "#78787822", "#4d004b67"),
+                      c("down", "up", "opposite", "no change", "condition specific"))
+
+    rdf = inner_join(refdf %>% transmute(label, !! sym$x := stat),
+                     cmpdf %>% transmute(label, !! sym$y := stat)) %>%
+        filter((!! sym$x)^2 + (!! sym$y)^2 > thresh^2) %>%
+        mutate(quad = factor(case_when(
+                  abs(!! sym$x) > thresh*2 & abs(!! sym$y) < thresh ~ "condition specific",
+                  abs(!! sym$x) < thresh & abs(!! sym$y) > thresh*2 ~ "condition specific",
+                  abs(!! sym$x) < thresh | abs(!! sym$y) < thresh ~ "no change",
+                  sign(!! sym$x) > 0 & sign(!! sym$y) > 0 ~ "up",
+                  sign(!! sym$x) < 0 & sign(!! sym$y) < 0 ~ "down",
+                  TRUE ~ "opposite"
+               ), levels=names(colors)),
+               dist = sqrt(scale(!! sym$x, center=FALSE)^2 +
+                           scale(!! sym$y, center=FALSE)^2),
+               showlab = ifelse(topf(dist,0.33), TRUE, FALSE)) %>% # third of labels for top
+        group_by(quad) %>%
+            mutate(showlab = ifelse(topf(dist,0.66*0.25), TRUE, showlab)) %>% # others per quad
+        ungroup() %>%
+        mutate(shape = ifelse(label %in% hollow, 1, 16),
+               label = ifelse(showlab | label %in% hl, label, NA))
 
     ggplot(rdf, aes_string(x=cmp, y=ref)) +
-        geom_point() +
-        theme_classic() #todo: colors for up, down, inconsistent, select labels, etc
+        geom_vline(xintercept=0, size=1, color="grey", linetype="dashed") +
+        geom_hline(yintercept=0, size=1, color="grey", linetype="dashed") +
+        geom_point(aes(color=quad, shape=shape), size=2) +
+        scale_shape_identity() +
+        ggrepel::geom_label_repel(aes(label=label, color=quad), size=2,
+            label.size=NA, alpha=1, segment.alpha=0.3, min.segment.length=0,
+            fill="#ffffffc0", label.padding=0.1, max.iter=1e5) +
+        theme_classic() +
+        scale_color_manual(name="change", values=colors, drop=FALSE)
 }
 
-volcano = function(rname, hl=c()) {
-    res[[rname]] %>%
+volcano = function(rname, col, hl=c()) {
+    res %>%
+        filter(cond == rname) %>%
+        pull(!! rlang::sym(col)) %>%
+        `[[`(1) %>%
         mutate(size = log10(baseMean + 1),
                circle = label %in% hl) %>%
         plt$p_effect("padj", "log2FoldChange") %>%
@@ -30,37 +64,19 @@ volcano = function(rname, hl=c()) {
 }
 
 args = sys$cmd$parse(
-    opt('e', 'eset', 'rds', '../data/rnaseq.rds'),
-    opt('o', 'outfile', 'rds', 'deseq.rds'),
-    opt('p', 'plotfile', 'pdf', 'deseq.pdf'),
-    arg('setfiles', 'rds', arity='*', '../data/genesets/human/CH.HALLMARK.rds')
+    opt('i', 'infile', 'rds', 'deseq.rds'),
+    opt('p', 'plotfile', 'pdf', 'rev_effect.pdf')
 )
 
-sets = sapply(args$setfiles, readRDS)
-names(sets) = basename(tools::file_path_sans_ext(names(sets)))
-
-eset = readRDS(args$eset)
-colData(eset)$cond = ifelse(grepl("il6", colData(eset)$treatment), "wt+il6",
-                            as.character(colData(eset)$genotype))
-colData(eset)$cond = relevel(factor(colData(eset)$cond), "wt")
-colData(eset)$rev = ifelse(grepl("rev", colData(eset)$treatment), 1, 0)
-
-design(eset) = ~ cond:rev
-res = DESeq(eset)
-res = resultsNames(res) %>%
-    setdiff("Intercept") %>%
-    sapply(util$extract_result, res=res, simplify=FALSE)
-res = tibble(cond = sub("\\.rev", "", sub("^cond", "", names(res))),
-             genes = unname(res))
-
+res = readRDS(args$infile)
 
 cmp = tidyr::crossing(tibble(ref = res$cond),
                       tibble(cmp = res$cond)) %>%
     rowwise() %>%
-        mutate(plot = list(volcano_or_compare(ref, cmp))) %>%
+        mutate(plot = list(volcano_or_compare(ref, cmp, "genes"))) %>%
     ungroup()
 
-plots = wrap_plots(cmp$plot, ncol=length(res))
+plots = wrap_plots(cmp$plot, ncol=nrow(res), guides="collect")
 
 pdf("rev_effect.pdf", 50, 50)
 print(plots)
