e647b31ac2771c7633ec2f17fa00b83a0b61ec51
mschubertv <mschu.dev@gmail.com>
2020-12-31 16:51:35 +0000

    add gsval

--- data/gsva.r
+++ data/gsva.r
@@ -6,3 +6,10 @@ args = sys$cmd$parse(
     opt('s', 'setfile', 'rds', 'genesets/human/MSigDB_Hallmark_2020.rds'),
     opt('o', 'outfile', 'rds', 'gsva/MSigDB_Hallmark_2020.rds')
 )
+
+eset = readRDS(args$eset)
+sets = readRDS(args$setfile)
+
+scores = GSVA::gsva(expr=expr, gset.idx.list=sets, parallel.sz=0)
+
+saveRDS(t(scores), file=args$outfile)
