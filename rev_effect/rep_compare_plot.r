bbf786328a3a3191fdec597e300e4625af8a0321
mschubertv <mschu.dev@gmail.com>
2021-01-04 14:04:47 +0000

    add condition as title

--- rev_effect/rep_compare_plot.r
+++ rev_effect/rep_compare_plot.r
@@ -3,7 +3,8 @@ library(DESeq2)
 library(ggplot2)
 library(patchwork)
 sys = import('sys')
-plt = import('./plot')
+plt = import('plot')
+plt2 = import('./plot')
 
 do_compare = function(res24, res48, .cond) {
     subs = . %>%
@@ -13,13 +14,17 @@ do_compare = function(res24, res48, .cond) {
     subs48 = subs(res48)
     conds = subs24$cond
 
+    do_plot = function(df, col, time)
+        plt2$compare(df, ref=conds[1], cmp=conds[2], col=col, thresh=1.5) +
+            labs(title=col, subtitle=time)
+
     plots = list(
-        plt$compare(subs24, conds[1], conds[2], "genes") + ggtitle("genes 24h"),
-        plt$compare(subs24, conds[1], conds[2], "MSigDB_Hallmark_2020") + ggtitle("Hallmarks 24h"),
-        plt$compare(subs48, conds[1], conds[2], "genes") + ggtitle("genes 48h"),
-        plt$compare(subs48, conds[1], conds[2], "MSigDB_Hallmark_2020") + ggtitle("Hallmarks 48h")
+        do_plot(subs24, "genes", "24"),
+        do_plot(subs24, "MSigDB_Hallmark_2020", "24"),
+        do_plot(subs48, "genes", "48"),
+        do_plot(subs48, "MSigDB_Hallmark_2020", "48")
     )
-    patchwork::wrap_plots(plots)
+    plt$text(.cond, size=10) / wrap_plots(plots, guides="collect") + plot_layout(ncol=1, heights=c(1,20))
 }
 
 sys$run({
