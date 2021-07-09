4eb642a8280c3c7f7587ac6f37ee78862b6b291e
mschubertv <mschu.dev@gmail.com>
2021-07-09 12:51:56 +0000

    add anchors only if req'd

--- comp_list/eset.r
+++ comp_list/eset.r
@@ -2,9 +2,13 @@ library(dplyr)
 library(DESeq2)
 sys = import('sys')
 
-batch_anchors = function(has_batch=TRUE) {
-    if (!has_batch)
+batch_anchors = function(coldata_eset, rec) {
+    batches = left_join(as.data.frame(rec$samples), coldata_eset) %>%
+        group_by(treatment, genotype) %>% summarize(b = list(c(batch)))
+    needs_anchors = length(do.call(setdiff, batches$b)) != 0
+    if (!needs_anchors)
         return(tibble(sample_id="invalid", anchor=NA))
+
     batch_anchors = c(
         "SU_BT549_08_S15"=1, "SU_BT549_22_S29"=1, # batch 1, stat1+dmso
         "SU_CH210215_17_S35"=1, # batch 4, stat1+dmso
@@ -18,27 +22,34 @@ batch_anchors = function(has_batch=TRUE) {
 }
 
 make_eset = function(rec) {
-    ba = batch_anchors(grepl("batch", rec$design))
+    ba = batch_anchors(as.data.frame(colData(eset)), rec)
     keep = as.data.frame(rec$samples) %>% mutate(in_comparison=1) %>%
         right_join(as.data.frame(colData(eset))) %>%
         filter(!is.na(in_comparison) | sample_id %in% ba$sample_id) %>%
         left_join(ba) %>%
-        mutate(anchor = factor(ifelse(is.na(anchor), "0", anchor)))
+        mutate(anchor = factor(ifelse(is.na(anchor) | !is.na(in_comparison), "0", anchor)))
+
+    # if the batches of control and condition are different, add anchors
+    if (any(keep$anchor != "0"))
+        rec$design = sub("batch", "batch + anchor", rec$design, fixed=TRUE)
+
     eset2 = eset[,keep$sample_id]
     colData(eset2) = DataFrame(keep)
 
+    # make sure factor levels reflect the control/condition of the comparison file
     for (v in c("batch", "genotype", "treatment")) {
         colData(eset2)[[v]] = droplevels(factor(colData(eset2)[[v]]))
         if (v %in% names(rec$samples))
             colData(eset2)[[v]] = relevel(colData(eset2)[[v]], rev(rec$samples[[v]])[1])
     }
+
+    # remove genotype/treatment coefficients if they are already covered by anchors
     for (v in c("genotype", "treatment")) {
-        # todo: if an anchor is in the comparison, remove the anchor column
         colData(eset2)[[v]][is.na(eset2$in_comparison)] = levels(colData(eset2)[[v]])[1]
         colData(eset2)[[v]] = droplevels(factor(colData(eset2)[[v]]))
     }
-    design(eset2) = as.formula(sub("batch", "batch + anchor", rec$design, fixed=TRUE))
 
+    design(eset2) = as.formula(rec$design)
     mm = model.matrix(design(eset2), colData(eset2))
     cmp = cbind(keep[c("batch", "genotype", "treatment")], mm) %>%
         as.data.frame() %>% tibble::rownames_to_column("sample_id") %>% as_tibble()
