9f5bedf8b7dc842c6e63c1ec5ea291b893495339
mschubertv <mschu.dev@gmail.com>
2021-07-25 19:22:06 +0000

    fix log2FC, cutoffs

--- compare_two/1-rev_cgas_wt.r
+++ compare_two/1-rev_cgas_wt.r
@@ -28,25 +28,25 @@ cmp_sets = res %>%
     select(cond=comparison, MSigDB_Hallmark_2020, DoRothEA, CIN) %>%
     tidyr::gather("collection", "stats", -cond) %>%
     tidyr::unnest() %>%
-    filter(collection != "DoRothEA" | size >= 50) %>%
+#    filter(collection != "DoRothEA" | size >= 50) %>%
+    filter(size >= 50) %>%
     mutate(label = sub(" (a)", "", label, fixed=TRUE))
 
 show_lab = c(
     "Interferon Gamma Response", "Interferon Alpha Response", "TNF-alpha Signaling via NF-kB",
-    "E2F Targets", "G2-M Checkpoint", "Apoptosis", "Myc Targets V1", "UV Response Up",
-    "Inflammatory Response", "KRAS Signaling Up",
-    "Epithelial Mesenchymal Transition", "Estrogen Response Late",
+    "E2F Targets", "G2-M Checkpoint", "Myc Targets V1",
     "STAT1", "RELA", "E2F4", "NFKB1", "STAT3", "JUN", "FOS", "CREB1"
 )
 sres2 = cmp_sets %>%
     filter(cond != "diff") %>%
-    select(cond, collection, label, size, estimate) %>%
-    tidyr::pivot_wider(names_from=c(cond), values_from=c(estimate)) %>%
+    select(cond, collection, label, size, log2FoldChange) %>%
+    tidyr::pivot_wider(names_from=c(cond), values_from=c(log2FoldChange)) %>%
     left_join(cmp_sets %>% filter(cond == "diff") %>% select(label, p.value, adj.p)) %>%
-    mutate(label = ifelse(adj.p < 1e-5 | label %in% show_lab, label, NA),
-           min_p = cut(adj.p, breaks=c(0, 1e-5, 0.05, Inf), labels=c("<1e-5", "<0.05", "n.s.")))
+    mutate(label = ifelse(adj.p < 1e-10 | label %in% show_lab, label, NA),
+           min_p = cut(adj.p, breaks=c(0, 1e-10, 0.05, Inf), labels=c("<1e-10", "<0.01", "n.s.")))
 
-arr = c("Interferon Gamma Response", "Interferon Alpha Response", "STAT1")
+arr = c("Interferon Gamma Response", "Interferon Alpha Response", "STAT1",
+        "TNF-alpha Signaling via NF-kB", "Myc Targets V1")
 arts = c("cGas-independent", "cGas-dependent")
 arrws = sres2 %>%
     filter(label %in% arr) %>%
@@ -58,9 +58,9 @@ cairo_pdf(args$plotfile, 9, 7)
 ggplot(sres2, aes(x=x, y=y)) +
     geom_hline(yintercept=0, color="darkgrey", linetype="dotted") +
     geom_vline(xintercept=0, color="darkgrey", linetype="dotted") +
-    geom_abline(slope=c(1,0.83), size=2, color=c("grey90", "grey70"), linetype="dashed") +
+    geom_abline(slope=1, size=2, color="grey", linetype="dashed") +
     geom_point(aes(size=size, fill=collection, alpha=min_p), shape=21) +
-    scale_alpha_manual(values=c("<1e-5"=0.9, "<0.05"=0.4, "n.s."=0.1)) +
+    scale_alpha_manual(values=c("<1e-10"=0.9, "<0.01"=0.4, "n.s."=0.1)) +
     scale_size_area() +
     geom_segment(data=arrws, aes(x=from, xend=to, y=y, yend=y, color=set),
                  arrow = arrow(length = unit(0.01, "npc"), type="closed")) +
@@ -68,8 +68,8 @@ ggplot(sres2, aes(x=x, y=y)) +
     ggrepel::geom_label_repel(aes(label=label), size=3, max.iter=1e5, label.size=NA,
         min.segment.length=0, max.overlaps=Inf, segment.alpha=0.3, fill="#ffffffc0",
         label.padding=unit(0.2, "lines")) +
-    geom_text(data=data.frame(x=0.5, y=0.5, txt="more cGas-independent ⇖    ⇘ more cGas-dependent  "),
-              aes(x=x, y=y, label=txt), inherit.aes=FALSE, size=3.5, fontface="bold", color="#757575") +
+#    geom_text(data=data.frame(x=0.5, y=0.5, txt="more cGas-independent ⇖    ⇘ more cGas-dependent  "),
+#              aes(x=x, y=y, label=txt), inherit.aes=FALSE, size=3.5, fontface="bold", color="#757575") +
     theme_classic() +
     labs(x = "log2 FC wt: rev vs. DMSO",
          y = "log2 FC cGas KO: rev vs. DMSO",
