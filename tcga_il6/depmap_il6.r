1ae62c98b9a51ff3b6fd22df601e7593140fd294
mschubertv <mschu.dev@gmail.com>
2021-07-31 15:50:12 +0000

    iterate

--- tcga_il6/depmap_il6.r
+++ tcga_il6/depmap_il6.r
@@ -5,7 +5,7 @@ library("ExperimentHub")
 gset = import('genesets')
 seq = import('seq')
 
-long2wide = function(df, field) {
+long2wide = function(meta, df, field) {
     wide = df %>%
         group_by(cell_line, gene_name) %>%
             summarize(value = mean(!! rlang::sym(field))) %>%
@@ -17,47 +17,54 @@ long2wide = function(df, field) {
 }
 
 test_one = function(mat, mcol, dmat, dcol) {
+    dset = meta %>%
+        inner_join(tibble(cell_line=colnames(mat), gene=mat[mcol,])) %>%
+        inner_join(tibble(cell_line=colnames(dmat), dep=dmat[dcol,]))
+
+    lm(dep ~ gene, data=dset) %>% broom::tidy() %>%
+        filter(term != "(Intercept)") %>%
+        arrange(p.value)
 }
-test_one(scores, "TNF-alpha Signaling via NF-kB", komat, "")
 
-eh = ExperimentHub()
-query(eh, "depmap")
+sys$run({
+    eh = ExperimentHub()
+    query(eh, "depmap")
 
-meta = depmap::depmap_metadata()
-rnai = depmap::depmap_rnai()
-ko = depmap::depmap_crispr()
-gex = depmap::depmap_TPM()
-aneup = readr::read_csv("aneuploidy_scores.csv") %>%
-    dplyr::rename(depmap_id = DepMap_ID)
+    rnai = depmap::depmap_rnai()
+    ko = depmap::depmap_crispr()
+    gex = depmap::depmap_TPM()
+    aneup = readr::read_csv("aneuploidy_scores.csv") %>%
+        dplyr::rename(depmap_id = DepMap_ID)
 
-aneup2 = readr::read_csv("CCLE_segment_cn.csv") %>%
-    mutate(width = End - Start, ploidy = Segment_Mean*2) %>%
-    seq$aneuploidy(seqnames="Chromosome", sample="DepMap_ID") %>%
-    select(depmap_id=DepMap_ID, aneup=aneuploidy)
+    aneup2 = readr::read_csv("CCLE_segment_cn.csv") %>%
+        mutate(width = End - Start, ploidy = Segment_Mean*2) %>%
+        seq$aneuploidy(seqnames="Chromosome", sample="DepMap_ID") %>%
+        select(depmap_id=DepMap_ID, aneup=aneuploidy)
 
-hms = gset$get_human("MSigDB_Hallmark_2020")
-gex_mat = long2wide(gex, "rna_expression")
-scores = GSVA::gsva(gex_mat, hms) #, parallel.sz=30)
-sdf = as_tibble(t(scores)) %>%
-    mutate(cell_line = colnames(scores))
+    hms = gset$get_human("MSigDB_Hallmark_2020")
+    gex_mat = long2wide(gex, "rna_expression")
+    scores = GSVA::gsva(gex_mat, hms) #, parallel.sz=30)
+    sdf = as_tibble(t(scores)) %>%
+        mutate(cell_line = colnames(scores))
 
-rnaimat = long2wide(rnai, "dependency")
-komat = long2wide(ko, "dependency")
+    rnaimat = long2wide(rnai, "dependency")
+    komat = long2wide(ko, "dependency")
 
-# RPPA: STAT3_pY705 STAT3_Caution NF-kB-p65_pS536_Caution
+    # RPPA: STAT3_pY705 STAT3_Caution NF-kB-p65_pS536_Caution
 
-#df = inner_join(rnai %>% filter(gene_name == "MB21D1") %>% select(cell_line, dependency),
-df = inner_join(ko %>% filter(gene_name == "MAD2L1") %>% select(cell_line, dependency),
-                gex %>% filter(gene_name == "IL6R") %>% select(cell_line, rna_expression)) %>%
-    inner_join(meta %>% filter(primary_disease == "Breast Cancer")) %>%
-    inner_join(aneup) %>%
-    inner_join(aneup2) %>%
-    left_join(sdf)
+    meta = depmap::depmap_metadata() %>%
+        left_join(aneup) %>%
+        left_join(aneup2)
 
-lm(dependency ~ rna_expression, data=df) %>% broom::tidy() %>%
-    arrange(p.value)
+    res = tidyr::crossing(tibble(set=rownames(scores)), tibble(dep=c("CGAS", "IL6R", "IL6ST"))) %>%
+        rowwise() %>%
+            mutate(res = list(test_one(scores, set, komat, dep))) %>%
+        ungroup() %>%
+        tidyr::unnest() %>%
+        mutate(adj.p = p.adjust(p.value, method="fdr")) %>%
+        arrange(adj.p, p.value)
 
-
-ggplot(df, aes(x=rna_expression, y=dependency)) +
-    geom_point(aes(size=`Aneuploidy score`, color=lineage_sub_subtype)) +
-    ggrepel::geom_text_repel(aes(label=cell_line), size=3)
+    ggplot(df, aes(x=rna_expression, y=dependency)) +
+        geom_point(aes(size=`Aneuploidy score`, color=lineage_sub_subtype)) +
+        ggrepel::geom_text_repel(aes(label=cell_line), size=3)
+})
