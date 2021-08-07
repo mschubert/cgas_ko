2e40e746d9049a13b01dcce468feeba5b99c15a0
mschubertv <mschu.dev@gmail.com>
2021-08-07 19:48:29 +0000

    minor text size tweak

--- tcga_il6/il6r-aneup.r
+++ tcga_il6/il6r-aneup.r
@@ -93,13 +93,14 @@ scatter_with_correction = function(df, x, ys, cor) {
     ggplot(comb, aes(x=!! rlang::sym(x), y=!! rlang::sym("expression"))) +
         geom_point(aes(shape=factor(HER2), fill=factor(ER_PR)), alpha=0.1) +
         geom_smooth(method="lm", se=FALSE) +
-        ggpmisc::stat_fit_glance(method="lm", geom="text_npc", size=3, label.x=0.15,
+        ggpmisc::stat_fit_glance(method="lm", geom="text_npc", size=3.5, label.x=0.1,
                                  aes(label = paste("p", signif(..p.value.., digits = 2)))) +
         scale_shape_manual(values=c("0"=21, "1"=24), na.value=22) +
         scale_fill_manual(values=c("0"="red", "1"="blue"), na.value="black") +
         guides(shape = guide_legend("HER2 amp", override.aes = list(size=2, alpha=0.5)),
                fill = guide_legend("ER/PR status", override.aes = list(size=2, alpha=0.5, shape=21))) +
         theme_classic() +
+        theme(strip.text = element_text(size=10)) +
         facet_grid(gene ~ type, scales="free", switch="y")
 }
 
