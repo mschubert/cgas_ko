db4c33bf5c6e81a14e72ebd5d54f15190313157f
mschubert <mschu.dev@gmail.com>
2022-01-21 23:46:48 +0100

    add rev: cgas vs wt

--- report/supp_rnaseq.r
+++ report/supp_rnaseq.r
@@ -5,11 +5,13 @@ plt = import('plot')
 
 wt = "../comp_list/cmp/BT549 WT Reversine vs DMSO.xlsx"
 cgas = "../comp_list/cmp/BT549 cGAS KO Reversine vs DMSO.xlsx"
+cmp = "../comp_list/cmp/BT549 cGAS KO reversine vs BT549 WT reversine.xlsx"
 sheets = c("genes", "MSigDB_Hallmark_2020", "DoRothEA")
 
 res = list(
     wt = sapply(sheets, function(s) readxl::read_excel(wt, sheet=s)),
-    cgas = sapply(sheets, function(s) readxl::read_excel(cgas, sheet=s))
+    cgas = sapply(sheets, function(s) readxl::read_excel(cgas, sheet=s)),
+    cmp = sapply(sheets, function(s) readxl::read_excel(cmp, sheet=s))
 )
 
 wt = list(
@@ -24,8 +26,14 @@ cgas = list(
     plt$volcano(res$cgas[[2]], p=0.05, label_top=20) + labs(subtitle="MSigDB Hallmarks", y="Adjusted p-value (FDR)"),
     plt$volcano(res$cgas[[3]], p=0.05, label_top=20) + labs(subtitle="DoRothEA TF regulons", y="Adjusted p-value (FDR)")
 )
-volcs = wrap_plots(wt) / wrap_plots(cgas)
+cmp = list(
+    plt$volcano(res$cmp[[1]], p=0.25, label_top=40) +
+        labs(title="BT549 Reversine: cGAS KO vs. WT", subtitle="Genes", y="Adjusted p-value (FDR)"),
+    plt$volcano(res$cmp[[2]], p=0.05, label_top=20) + labs(subtitle="MSigDB Hallmarks", y="Adjusted p-value (FDR)"),
+    plt$volcano(res$cmp[[3]], p=0.05, label_top=20) + labs(subtitle="DoRothEA TF regulons", y="Adjusted p-value (FDR)")
+)
+volcs = wrap_plots(wt) / wrap_plots(cgas) / wrap_plots(cmp)
 
-pdf("supp_rnaseq.pdf", 14, 14)
+pdf("supp_rnaseq.pdf", 14, 21)
 print(volcs)
 dev.off()
