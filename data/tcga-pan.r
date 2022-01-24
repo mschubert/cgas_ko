d9b9ef3aa5219997602dfc9ea7fa8e4ddf56b6b8
mschubertv <mschu.dev@gmail.com>
2022-01-24 12:28:39 +0000

    add gene expr

--- data/tcga-pan.r
+++ data/tcga-pan.r
@@ -3,15 +3,24 @@ sys = import('sys')
 tcga = import('data/tcga')
 idmap = import('process/idmap')
 
+gex_tmm = function(cohort, genes) {
+    re = tcga$rna_seq(cohort, trans="tmm")[genes,]
+    rownames(re) = names(genes)
+    re
+}
+
 sys$run({
     args = sys$cmd$parse(
         opt('o', 'outfile', 'rds', 'tcga-pan.rds')
     )
 
-    pur2 = tcga$purity_estimate() %>% select(Sample, cohort, purity) # aran2015 different(!)
-    incl = sort(unique(pur2$cohort))
+    incl = c("BRCA", "LUAD", "LUSC", "OV", "COAD", "SKCM")
+    genes = c(IL6R="ENSG00000160712", IL6="ENSG00000136244", IL6ST="ENSG00000134352", CGAS="ENSG00000164430")
+    hms = c("E2F Targets", "Interferon Gamma Response", "IL-6/JAK/STAT3 Signaling")
+    CIN = c("CIN70_Carter2006")
+
     pur = tcga$purity_aran2015() %>% select(Sample, cohort, purity=estimate) %>%
-        filter(cohort %in% incl)
+        filter(cohort %in% incl, substr(Sample, 14, 16) == "01A")
 
     meta = tcga$clinical() %>%
         transmute(patient = submitter_id,
@@ -29,8 +38,9 @@ sys$run({
                                  days_to_last_follow_up,
                                  na.rm=TRUE))
 
-    scores = c(lapply(incl, tcga$gsva, setname="MSigDB_Hallmark_2020"),
-               lapply(incl, tcga$gsva, setname="CIN")) %>%
+    scores = c(lapply(incl, gex_tmm, genes=genes),
+               lapply(incl, function(i) tcga$gsva(i, "MSigDB_Hallmark_2020")[hms,]),
+               lapply(incl, function(i) tcga$gsva(i, "CIN")[CIN,,drop=FALSE])) %>%
         narray::stack(along=2) %>% t() %>%
         as.data.frame() %>%
         tibble::rownames_to_column("Sample") %>%
@@ -43,11 +53,7 @@ sys$run({
         mutate(patient = substr(Sample, 1, 12)) %>%
         inner_join(meta) %>%
         left_join(aneup) %>%
-        left_join(scores %>% select(Sample,
-                                    `CIN70_Carter2006`,
-                                    `E2F Targets`,
-                                    `Interferon Gamma Response`,
-                                    `IL-6/JAK/STAT3 Signaling`)) %>%
+        left_join(scores) %>%
         select(-patient)
 
     saveRDS(dset, file=args$outfile)
