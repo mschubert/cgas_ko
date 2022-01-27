eba0ba23e9578759521e276dc6ea68e6e534c139
mschubert <mschu.dev@gmail.com>
2022-01-27 23:25:38 +0100

    make more compact

--- report/main_TCGA-pan.r
+++ report/main_TCGA-pan.r
@@ -35,55 +35,67 @@ dset_surv = left_join(pan, cors) %>%
 dset_expr = dset_surv %>%
     select(Sample, cohort, aneuploidy, CGAS, IL6, IL6R) %>%
     filter(!is.na(aneuploidy), cohort != "BRCA") %>%
-    mutate(aneuploidy = cut(aneuploidy, c(-Inf,0.1,Inf), labels=c("low", "high"))) %>%
     tidyr::gather(field, value, -aneuploidy,  -Sample, -cohort) %>%
-    group_by(cohort) %>%
-        mutate(n_cohort = n()) %>%
-    group_by(cohort, aneuploidy) %>%
-        filter(n() > 0.05*n_cohort) %>%
     group_by(field) %>%
         mutate(value = scale(value)) %>%
     ungroup()
 
-plot_cohort = function(.cohort) {
-    if (.cohort %in% c("OV", "SKCM", "COAD"))
+plot_cohort = function(.cohort, aneup_thresh=0.2) {
+    if (identical(.cohort, c("OV")))
         dset_surv$il6_cor = dset_surv$il6_bias
 
-    dc = dset_surv %>% filter(cohort == .cohort) %>%
-        mutate(il6_cor2 = cut(il6_cor, labels=c("low", "middle", "high"),
-               c(-Inf, quantile(il6_cor, 0.25, na.rm=T), quantile(il6_cor, 0.75, na.rm=T), Inf)))
+    dc = dset_surv %>% filter(cohort %in% .cohort) %>%
+        mutate(il6_cor2 = cut(il6_cor, labels=c("low", "high"),
+               c(-Inf, quantile(il6_cor, 0.25, na.rm=T), Inf)))
 
-    top = dset_expr %>%
-        filter(cohort == .cohort) %>%
-        ggplot(aes(x=field, y=value, color=aneuploidy, fill=field)) +
+    pe = dset_expr %>%
+        filter(cohort %in% .cohort) %>%
+        ggplot(aes(x=field, y=value, fill=field)) +
             geom_hline(yintercept=0, linetype="dotted", size=2, alpha=0.5) +
             geom_boxplot(outlier.shape=NA, alpha=0.9, position=position_dodge2(preserve="single")) +
             scale_x_discrete(drop=FALSE) +
-            theme_minimal() +
-            coord_cartesian(ylim=c(-2.5,2.5))
+            theme_minimal() + theme(axis.text.x=element_blank()) +
+            coord_cartesian(ylim=c(-2.5,2.5)) +
+            labs(fill="Gene")
 
-    p_il6 = . %>% filter(term=="il6_cor") %>% pull(p.value)
+    p_il6 = function(x) {
+        re = x %>% filter(term=="il6_cor") %>% pull(p.value)
+        ifelse(re<0.05, sprintf("p=%.2g", re), "p=n.s.")
+    }
 
-    dca = dc %>% filter(aneuploidy > 0.1)
+    dca = dc %>% filter(aneuploidy > aneup_thresh)
     m1 = coxph(Surv(os_years, vital_status) ~ age_days + purity + il6_cor, data=dca) %>% broom::tidy()
     fit = survfit(Surv(os_years, vital_status) ~ il6_cor2, data=dca)
-    surv1 = ggsurvplot(fit, data=dca, xlim=c(0,5), break.time.by=2.5) +
-        ylab("OS aneuploid")
-    p1 = annotate("text_npc", npcx=0.1, npcy=0.1, label=sprintf("p=%.2g", p_il6(m1)))
+    surv1 = ggsurvplot(fit, data=dca, xlim=c(0,5), break.time.by=5, censor.size=3,
+                       legend.title="IL6 (cancer)", legend.labs=c("low","high"))
+    p1 = list(scale_color_manual(values=c(low="grey", high="#ad07e3"), drop=FALSE),
+              scale_y_continuous(breaks=c(0,1)),
+              annotate("text_npc", npcx=0.1, npcy=0.1, label=p_il6(m1)))
 
-    dce = dc %>% filter(aneuploidy < 0.1)
+    dce = dc %>% filter(aneuploidy < aneup_thresh)
     m2 = coxph(Surv(os_years, vital_status) ~ age_days + purity + il6_cor, data=dce) %>% broom::tidy()
     fit = survfit(Surv(os_years, vital_status) ~ il6_cor2, data=dce)
-    surv2 = ggsurvplot(fit, data=dce, xlim=c(0,5), break.time.by=2.5) +
-        ylab("OS euploid")
-    p2 = annotate("text_npc", npcx=0.1, npcy=0.1, label=sprintf("p=%.2g", p_il6(m2)))
+    surv2 = ggsurvplot(fit, data=dce, xlim=c(0,5), break.time.by=5, censor.size=3,
+                       legend.title="IL6 (cancer)", legend.labs=c("low","high"))
+    p2 = list(scale_color_manual(values=c(low="grey", high="#ad07e3"), drop=FALSE),
+              scale_y_continuous(breaks=c(0,1)),
+              annotate("text_npc", npcx=0.1, npcy=0.1, label=p_il6(m2)))
 
-    (plt$text(.cohort, size=5) / top / (surv1$plot + p1) / (surv2$plot + p2)) +
-        plot_layout(heights=c(0.08,1,1,1))
+    list(plt$text(paste(.cohort, collapse="/"), size=5, angle=90), pe, surv1$plot + p1, surv2$plot + p2)
 }
 
-plots = lapply(c("LUAD", "LUSC", "OV", "SKCM", "COAD"), plot_cohort)
+cohorts = list(c("LUAD", "LUSC"), "OV", "SKCM")
+plots = lapply(cohorts, plot_cohort)
+wp = c(list(plot_spacer(), plt$text("Expression\nz-score") + coord_cartesian(clip="off"),
+            plt$text("Aneuploidy high"), plt$text("Aneuploidy low")),
+       do.call(c, plots))
+asm = wrap_plots(wp, ncol=4, widths=c(1,3,5,5), heights=c(1.5,rep(5,length(cohorts)))) + plot_layout(guides="collect") &
+    theme(axis.title.x = element_blank(),
+          axis.title.y = element_blank(),
+          legend.direction = "vertical",
+          legend.title = element_text(size=12),
+          legend.text = element_text(size=10))
 
-pdf("main_TCGA-pan.pdf", 16, 9)
-wrap_plots(plots, nrow=1) + plot_layout(guides="collect") & theme(legend.direction = "vertical")
+pdf("main_TCGA-pan.pdf", 6.5, 6.5)
+print(asm)
 dev.off()
